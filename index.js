fc239897e7b4e3be90858ef1c6e2a1c37fd68148
ch-folio <97145152+ch-folio@users.noreply.github.com>
2023-04-11 16:00:15 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,20 +3,6 @@ const express = require("express");
 const app = express();
 var fs = require("fs");
 
-let ServerOrderArray = [];
-
-fileManager = {
-  read: function () {
-    const stat = fs.statSync("orderData.json");
-    var rawdata = fs.readFileSync("orderData.json");
-    ServerOrderArray = JSON.parse(rawdata);
-  },
-  write: function () {
-    let data = JSON.stringify(ServerOrderArray);
-    fs.writeFileSync("orderData.json", data);
-  },
-};
-// serve files from the public directory
 app.use(express.static("public"));
 app.use(express.json());
 
@@ -24,14 +10,22 @@ app.listen(3000, () => {
   console.log("listening on 3000");
 });
 
-// serve the homepage
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-//serve the submit 1 order
 app.post("/submitOrder1", (req, res) => {
   const order = req.body;
   console.log("Received order:", order);
-  res.json({ message: "Order added successfully." });
+  // res.json({ message: "Order added successfully." });
+
+  const orderStrValue = JSON.stringify(order) + "\n";
+  fs.appendFile("orders.txt", orderStrValue, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Failed to write order to file." });
+    } else {
+      res.json({ message: "Order submitted and written to file." });
+    }
+  });
 });
